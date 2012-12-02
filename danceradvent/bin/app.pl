188987c6d5c2e4217a42a91177b5d7e8fba99715
David Precious <davidp@preshweb.co.uk>
2012-12-02 12:13:25 +0000

    Look for Dancer2 in my homedir.
    Submodules aren't working well for me right now, and I don't have time to
    troubleshoot properly.

--- danceradvent/bin/app.pl
+++ danceradvent/bin/app.pl
@@ -6,6 +6,11 @@ BEGIN {
     while ( my $libdir = glob("${FindBin::Bin}/../vendor/*/lib") ) {
         unshift @INC, $libdir;
     }
+
+    # Bodge for production box, where git isn't supporting submodules correctly:
+    for my $repo (qw(Dancer2 Dancer-Plugin-Feed)) {
+        unshift @INC, "/home/davidp/$repo/lib";
+    }
 }
 
 use Dancer 2.0;
